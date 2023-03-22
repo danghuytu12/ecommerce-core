3263dff463eaa2b91401bfe35b9db9a66abbe109
Huytu <danghuytu.thd2006@gmail.com>
2023-03-23 00:46:29 +0700

    fix: config app

--- app.js
+++ app.js
@@ -3,52 +3,27 @@ const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
+const productsRouter = require('./routes/products')
+const categoryRouter = require('./routes/category')
+const userRouter = require('./routes/user')
+const orderRouter = require('./routes/order')
+const api = '/api/v1'
 // middleware
 
 app.use(bodyParser.json())
 app.use(morgan('tiny'))
+app.use(cors())
+app.options('*', cors())
 
+app.use(`/api/v1/products`, productsRouter)
+app.use(`/api/v1/categories`, categoryRouter)
+app.use(`/api/v1/users`, userRouter)
+app.use(`/api/v1/orders`, orderRouter)
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number
-})
-
-const Product = mongoose.model('Product', productSchema)
 
 require('dotenv/config')
-const api = process.env.API_URL
-
-
-app.get(`${api}/products`, (req, res) => {
-
-    const products = {
-        id: 1,
-        name: 'aaa',
-        image: ' adasdasd'
-    }
-    res.send(products)
-})
-
-app.post(`${api}/products`, (req, res) => {
-
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    })
-    product.save().then((create => {
-        res.status(200).json(create)
-    })).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-    res.send(product)
-})
 
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
